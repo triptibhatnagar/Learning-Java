7b3034feba9580fa898b7579d17cce3233692494
triptibhatnagar <bhatnagartripti2727@gmail.com>
2024-06-09 21:59:40 +0530

    Day2

---
+++ Day2/Conditions.java
@@ -0,0 +1,205 @@
+package Day2;
+
+import java.util.Scanner;
+
+public class Conditions {
+    public static void main(String args[]) {
+        /*
+        Conditional Statements -
+        if, if else, nested if else, else if, switch
+
+        -> if block is used to specify the code to be executed if the condition specified in if is true
+        -> else block is executed if condition is false
+        -> switch case statements are a substitute for many else if statements that compare a variable to multiple values. After a match is found,it executes the corresponding code of that value case.
+        */
+
+        //Input user's age and tell whether they are adult or not.
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter your age: ");
+        int age = sc.nextInt();
+
+        if(age>18) {
+            System.out.println("User is adult");
+        }else {
+            System.out.println("User is not adult");
+        }
+
+        
+        //Input a number and check whether it is odd or even
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+
+        if(n%2 == 0) {
+            System.out.println("Even number");
+        }else {
+            System.out.println("Odd number");
+        }
+
+
+        //Input 2 numbers, a and b, and tell whether both are equal, a is greater or a is smaller.
+        System.out.print("Enter two numbers : ");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        if(a == b) {
+            System.out.println("Both are equal");
+        }else if(a>b) {
+            System.out.println("a is greater");
+        }else {
+            System.out.println("a is smaller");
+        }
+
+        //nested if else
+        /*
+        if(a == b) {
+            System.out.println("Both are equal");
+        }else {
+            if(a>b) {
+                System.out.println("a is greater");
+            }else {
+                System.out.println("a is smaller");
+            }
+        }
+        */
+
+
+        //If 3 is a odd number, print "Bazinga"
+        if(3%2 == 1) 
+            System.out.println("Bazinga");
+        //single statement can be executed without {}
+
+
+        //Input a number from user and print "Hello" for 1, "Namaste" for 2, "Bonjour" for 3
+        System.out.print("Enter a number - 1,2 or 3: ");
+        int choice = sc.nextInt();
+
+        switch(choice) {
+            case 1: 
+            System.out.println("Hello");
+            break;
+            case 2: 
+            System.out.println("Namaste");
+            break;
+            case 3: 
+            System.out.println("Bonjour");
+            break;
+            default:
+            System.out.println("Invalid number");
+        }
+
+
+        //Make a calculator. Take 2 numbers(x and y) from the user and perform an operation as follows.
+        /* 
+        1 : + 
+        2 : -
+        3 : *
+        4 : /
+        5 : %
+        */
+
+        System.out.print("Enter two numbers: ");
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+
+        System.out.println("Enter 1 for addition, 2 for subtraction, 3 for multiplication, 4 for division, 5 for remainder");
+        int op = sc.nextInt();
+
+        switch(op) {
+            case 1 :
+            System.out.println(a+b);
+            break;
+            case 2 :
+            System.out.println(a-b);
+            break;
+            case 3 :
+            System.out.println(a*b);
+            break;
+            case 4 :
+            System.out.println(a/b);
+            break;
+            case 5 :
+            System.out.println(a%b);
+            break;
+            default: 
+            System.out.println("Invalid Operator");
+        }
+
+
+        //Ask the user to enter the number of the month and print the name of the month.
+        System.out.print("Enter the month number: ");
+        int month = sc.nextInt();
+
+        switch(month) {
+            case 1:
+            System.out.println("January");
+            break;
+            case 2:
+            System.out.println("February");
+            break;
+            case 3:
+            System.out.println("March");
+            break;
+            case 4:
+            System.out.println("April");
+            break;
+            case 5:
+            System.out.println("May");
+            break;
+            case 6:
+            System.out.println("June");
+            break;
+            case 7:
+            System.out.println("July");
+            break;
+            case 8:
+            System.out.println("August");
+            break;
+            case 9:
+            System.out.println("September");
+            break;
+            case 10:
+            System.out.println("October");
+            break;
+            case 11:
+            System.out.println("November");
+            break;
+            case 12:
+            System.out.println("December");
+            break;
+            default: 
+            System.out.println("Invalid number");
+        }
+
+
+        //Ask the user to enter the number of the day of a week and print the name of the day.
+        System.out.print("Enter the day number: ");
+        int day = sc.nextInt();
+
+        switch(day) {
+            case 1:
+            System.out.println("Monday");
+            break;
+            case 2:
+            System.out.println("Tuesday");
+            break;
+            case 3:
+            System.out.println("Wednesday");
+            break;
+            case 4:
+            System.out.println("Thursday");
+            break;
+            case 5:
+            System.out.println("Friday");
+            break;
+            case 6:
+            System.out.println("Saturday");
+            break;
+            case 7:
+            System.out.println("Sunday");
+            break;
+            default :
+            System.out.println("Error 404..");
+        }
+    }
+}
---
+++ Day2/Loops.java
@@ -0,0 +1,135 @@
+package Day2;
+
+import java.util.Scanner;
+
+public class Loops {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        /*
+        LOOP -  A loop is used for executing a block of statements repeatedly until a particular condition is satisfied.
+        A loop consists of an initialization statement, a test condition and an increment statement.
+
+        FOR LOOP - The syntax of the for loop is :
+        for(initialization; condition; update) {
+            body of loop
+        }
+        */
+        //Example
+        System.out.println("For Loop :-");
+        for(int i=1; i<=10; i++) {
+            System.out.print(i+" ");
+        }
+
+        /*
+        WHILE LOOP - The syntax of the while loop is :
+        initialization;
+        while(condition) {
+            body of loop
+            update;
+        }
+        */
+        //Example
+        System.out.println("\nWhile Loop :-");
+        int counter = 1;
+        while(counter<=10) {
+            System.out.print(counter+" ");
+            counter++; //counter = counter+1
+        }
+
+        /*
+        DO WHILE LOOP - The syntax of the do while loop is :
+        initialization;
+        do {
+            body of loop
+        }while(condition);
+        */
+        System.out.println("\nDo While Loop :-");
+        int counter2 = 1;
+        do {
+            System.out.print(counter2+" ");
+            counter2++;
+        }while(counter2<=10);
+
+        /*
+        DIFFERNCE BETWEEN USAGE OF WHILE AND DO-WHILE
+        The main difference between the two loops is that the while loop checks the condition before the execution of the statement(s) whereas the do-while loop ensures that the statement(s) are executed at least once before evaluating the condition. While loop is entry-controlled and Do-while is exit controlled
+        */
+
+        //QUESTIONS
+
+        //Print all the even numbers till n.
+        System.out.print("\nEnter a number : ");
+        int n = sc.nextInt();
+
+        System.out.print("Even numbers till "+n+" : ");
+        for(int i = 0; i<=n ; i++) {
+            if(i%2 == 0) {
+                System.out.print(i+" ");
+            }
+        }
+
+        //Print the sum of first n natural numbers.
+        System.out.print("\nSum of numbers till "+n+" : ");
+        int sum = 0;
+        for(int i = 1; i<=n ; i++) {
+            sum += i;
+        }
+        System.out.print(sum);
+
+        //Print the table of a number n.
+        System.out.println("\nTable of "+n+" : ");
+        for(int i = 1; i<=10; i++) {
+            System.out.println(n+" * "+i+" = "+n*i);
+        }
+
+
+        //Print if a number n is prime or not.
+        boolean isPrime = true;
+    
+        for(int i = 2; i<n/2; i++) {
+            if(n%i == 0) {
+                isPrime = false;
+                break;
+            }
+        }
+        if(isPrime) {
+            if(n == 1 || n == 0) {
+                System.out.println("This is neither a prime nor a composite number");
+            }else {
+                System.out.println("This is a prime number");
+            }
+        }else {
+            System.out.println("This is not a prime number");
+        }
+
+        /*
+        INFINITE LOOP - 
+
+        Example - 
+        for(;;) {
+            System.out.println("Hello");
+        }
+
+        This loop will print Hello infinite times as there is no initialization, condition and updation paramters.
+        */
+
+        //Make a menu driven program. The user can enter 2 numbers, either 1 or 0. If the user enters 1, then keep taking input from the user for a student's marks (out of 100). If they enter 0, then stop. If student scores : marks >=90 -> "Great" , 89>=marks>=60 -> "Good" , 59>=marks>=0 -> "This is Good as well".
+
+        int choice;
+        do {
+            System.out.print("Enter the student's marks : ");
+            int marks = sc.nextInt();
+            
+            if(marks >= 90) {
+                System.out.println("Great");
+            }else if(89 >= marks && marks >= 60) {
+                System.out.println("Good");
+            }else {
+                System.out.println("This is Good as well");
+            }
+            System.out.println("Do you want to continue?\n1(Yes) or 0(No)");
+            choice = sc.nextInt();
+        }while(choice == 1);
+          
+    }
+}
